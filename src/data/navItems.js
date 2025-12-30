a321542d92eb4791b8cb3a3b167f0383d689a4c3
Nandu Krishna T <nandukrishnat3@gmail.com>
2025-12-30 12:00:16 +0530

    Initial Commit

---
+++ src/data/navItems.js
@@ -0,0 +1,6 @@
+export const navItems = [
+    { name: "Home", href: "/", id: "home" },
+    { name: "About Us", href: "/about-us", id: "about-us" },
+    { name: "Projects", href: "/projects", id: "projects" },
+    { name: "Contact Us", href: "/contact-us", id: "contact-us" },
+];
