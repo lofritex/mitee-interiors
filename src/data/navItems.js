a5572c9870cb5e4ccb82e2e507bd78f77f90caee
Nandu Krishna T <nandukrishnat3@gmail.com>
2026-01-01 18:43:04 +0530

    chore: added client changes

--- src/data/navItems.js
+++ src/data/navItems.js
@@ -1,6 +1,6 @@
 export const navItems = [
-    { name: "Home", href: "/", id: "home" },
-    { name: "About Us", href: "/about-us", id: "about-us" },
-    { name: "Projects", href: "/projects", id: "projects" },
-    { name: "Contact Us", href: "/contact-us", id: "contact-us" },
+    { name: "Home", href: "/", id: "home", showInHeader: true },
+    { name: "About Us", href: "/about-us", id: "about-us", showInHeader: true },
+    { name: "Projects", href: "/projects", id: "projects", showInHeader: true },
+    { name: "Contact Us", href: "/contact-us", id: "contact-us", showInHeader: true },
 ];
