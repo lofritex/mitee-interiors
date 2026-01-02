30516360f5f898fbcdf13827772970d45e5cf8f6
Nandu Krishna T <nandukrishnat3@gmail.com>
2026-01-02 19:02:36 +0530

    feat: add social media links

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,4 +1,9 @@
-export default {
-    phone:"+91 95616 77393",
-    email:"hello@example.com"
-}
+const data = {
+    phone: "+91 95616 77393",
+    email: "hello@example.com",
+    instagram: "https://www.instagram.com/miteeinteriors",
+    facebook: "https://www.facebook.com/share/1BTnSTiFWb/?mibextid=wwXIfr",
+    linkedin: "https://www.linkedin.com/company/mitee-interiorspune",
+};
+
+export default data;
