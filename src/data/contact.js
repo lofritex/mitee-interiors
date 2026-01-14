a55c225ab9b54124a1fc94da10f8d10390e77733
Nandu Krishna T <nandukrishnat3@gmail.com>
2026-01-14 14:09:38 +0530

    chore: changes from client

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,6 @@
 const data = {
     phone: "+91 95616 77393",
-    email: "hello@example.com",
+    email: "Info@miteeinteriors.com",
     instagram: "https://www.instagram.com/miteeinteriors",
     facebook: "https://www.facebook.com/share/1BTnSTiFWb/?mibextid=wwXIfr",
     linkedin: "https://www.linkedin.com/company/mitee-interiorspune",
