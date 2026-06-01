d6f40936f78d6080254ab65c00c9644037aff910
Nandu Krishna T <nandukrishnat3@gmail.com>
2026-06-01 17:41:50 +0530

    feat: added more contents and client reviews

---
+++ src/data/testimonials.ts
@@ -0,0 +1,22 @@
+export const testimonials: { content: string; author: string }[] = [
+    {
+        content:
+            "Thanks a lot for all the hard work. The house is really looking good. Touch wood. I really appreciate all the efforts and time you had given to our home. It was really good to work with you. The professionalism you carry is good and keep it up. We started our discussions in Oct-Nov and you had everything very clear in your mind (approach, Designs, Selections). I wish you all the very best and I am sure you would reach new heights for sure.",
+        author: "Abhishek & Kirti Hadap",
+    },
+    {
+        content:
+            "Creativity and simplicity at its best....!!! We selected Saanjali from the number of options that we had and when we saw the first design that she proposed, we knew that something great is coming. We trusted in her talent and she didn't let us down. Very unique design elements used for TV pannel, terrace garden, bathroom and bedrooms which we had never seen anywhere. Special thanks for making my dream terraces garden, a reality. Our relatives and friends just couldn't stop complimenting our house.",
+        author: "Hemant & Akshata Sonawane",
+    },
+    {
+        content:
+            "First of all thank you so much for creating my dream office just the way I wanted. I had so many ideas when I started thinking about the office interior and offcourse I shared all of my ideas with saanjali. What I really liked about her is that she actually considers your choice and ideas while designing. ",
+        author: "CA Mayur Tapkir",
+    },
+    {
+        content:
+            "Let's begin with our journey with Sanjali Recommended by: My parents as she took up their project too. We really loved the designs she presented and it provoked us to approach her. She carries unique ideas and recommended what's best for our space after understanding your needs. She has got a great choice of colour combination. To see the best out her you need to 'trust' her with her recommendation and the outcome is 120% going to be fantastic.",
+        author: "Vardhan and Sneha Kale",
+    },
+];
